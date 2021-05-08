bcb9cdebdd1cfba2c723f515fcd01d2dc85e7a99
leehm00 <leehm00@qq.com>
2021-05-08 10:56:00 +0800

    xiugai 1

--- lab1/template/merkle_tree.go
+++ lab1/template/merkle_tree.go
@@ -18,9 +18,48 @@ type MerkleNode struct {
 
 // NewMerkleTree creates a new Merkle tree from a sequence of data
 // implement
+func NewMerkleNode(left,right *MerkleNode, data []byte) *MerkleNode {
+    mNode := MerkleNode{}
+
+    if left == nil && right == nil {
+        hash := sha256.Sum256(data)
+        mNode.Data = hash[:]
+    }else {
+        prevHashes := append(left.Data,right.Data...)
+        hash := sha256.Sum256(prevHashes)
+        mNode.Data = hash[:]
+    }
+
+    mNode.Left = left
+    mNode.Right = right
+
+    return &mNode
+}
+
 func NewMerkleTree(data [][]byte) *MerkleTree {
-	var node = MerkleNode{nil,nil,data[0]}
-	var mTree = MerkleTree{&node}
+	var nodes []MerkleNode
+
+    if len(data) % 2 != 0 {
+        data = append(data, data[len(data) - 1])
+    }
+
+    for _, dataitem := range data {
+        node := NewMerkleNode(nil, nil, dataitem)
+        nodes = append(nodes, *node)
+    }
+
+    for i := 0; i<len(data)/2; i++ {
+        var newNodes []MerkleNode
+
+        for j := 0; j < len(nodes); j += 2 {
+            node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+            newNodes = append(newNodes , *node)
+        }
+
+        nodes = newNodes
+    }
+
+    mTree := MerkleTree{&nodes[0]}
 
 	return &mTree
 }
